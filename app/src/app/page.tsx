a777ae827143b376af5f9ced6764221c4209e737
Masahiro Suda <masahirosuda@EH-PC086.lan>
2024-06-02 13:59:15 +0900

    OpenAI DallE libsを追加

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -1,10 +1,12 @@
 "use client";
 import { testChatChainLLM } from "../libs/test";
+import { generateImageByOpenAiDalle } from "../libs/openAiDalle";
 
 export default function Home() {
   console.log("Hello, World!");
 
   // testChatChainLLM();
+  // generateImageByOpenAiDalle("かわいい白黒なねこ");
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
---
+++ app/src/libs/openAiDalle.ts
@@ -0,0 +1,19 @@
+import { DallEAPIWrapper } from "@langchain/openai";
+
+// OpenAIのAPIキーを設定します
+const apiKey = process.env.NEXT_PUBLIC_OPEN_AI_API_KEY ?? "";
+console.log("API Key:", apiKey);
+
+const dallE = new DallEAPIWrapper({
+  apiKey,
+  modelName: "dall-e-3",
+});
+
+export async function generateImageByOpenAiDalle(prompt: string) {
+  try {
+    const response = await dallE._call(prompt);
+    console.log("Generated image URL:", response);
+  } catch (error) {
+    console.error("Error generating image:", error);
+  }
+}
