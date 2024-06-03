6a30f931fe06b20507282958889afe59eae9f0db
Masahiro Suda <masahirosuda@EH-PC086.local>
2024-06-03 21:52:22 +0900

    Answer Page Component追加

---
+++ app/src/components/shared/ui-parts/header/Header.tsx
@@ -0,0 +1,13 @@
+export const Header = () => {
+  return (
+    <header>
+      <h1>意味教え太郎</h1>
+      <nav>
+        <ul>
+          <li>Home</li>
+          <li>Answer</li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
