423a499388c033fb9f8e33b1439e4f594f750316
Najah7 <najahmatsuo7@gmail.com>
2024-06-05 17:32:55 +0900

    ➕ [Add] font awesome

--- app/src/app/layout.tsx
+++ app/src/app/layout.tsx
@@ -1,6 +1,12 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-// import "./globals.css";
+
+
+import { config } from '@fortawesome/fontawesome-svg-core'
+import '@fortawesome/fontawesome-svg-core/styles.css'
+config.autoAddCss = false
+
+import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
