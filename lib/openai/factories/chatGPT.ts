13d38e94d5abb895910126c97f4856568271f7d8
Najah7 <najahmatsuo7@gmail.com>
2024-06-06 21:12:43 +0900

    🩹  [Fix] a little bit

--- lib/openai/factories/chatGPT.ts
+++ lib/openai/factories/chatGPT.ts
@@ -3,13 +3,14 @@ import { TemplateFactory } from "./templates"
 import { ChatGPT } from "../chatGPT/impl/chatGPT"
 import {  } from "openai"
 
-const OPENAI_API_KEY = process.env.NEXT_PUBLIC_OPEN_AI_API_KEY && process.env.OPENAI_API_KEY
+const next_api_key = process.env.NEXT_PUBLIC_OPEN_AI_API_KEY
+const OPENAI_API_KEY = next_api_key ? next_api_key : process.env.OPEN_AI_API_KEY
 
 // TODO: use DI and delete service factory
 // NOTE: I tried but DI is difficult to implement in TS or JS
 export const createChatGPT = () => {
     const openAI = new OpenAI({
-        apiKey: process.env.NEXT_PUBLIC_OPEN_AI_API_KEY,
+        apiKey: OPENAI_API_KEY,
     })
     const factory = new TemplateFactory()
     return new ChatGPT(openAI, factory)
